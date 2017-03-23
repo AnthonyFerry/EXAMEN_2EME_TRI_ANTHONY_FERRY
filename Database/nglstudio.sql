c73c0eba92d0be1885bb41a7daebc60c1ea45b48
AnthonyFerry <a.ferry@ludus-academie.fr>
2017-03-23 09:36:24 +0100

    mise a jour éléments bdd

--- Database/nglstudio.sql
+++ Database/nglstudio.sql
@@ -1,92 +1,98 @@
-DROP TABLE IF EXISTS CLIENT ;
-CREATE TABLE CLIENT (
-	id_client BIGINT  AUTO_INCREMENT NOT NULL,
-	prenom VARCHAR(16), 
-	nom VARCHAR(16), 
-	code_postal VARCHAR(5), 
-	ville VARCHAR(16), 
-	rue VARCHAR(64), 
-	PRIMARY KEY (id_client) ) ENGINE=InnoDB;  
+drop database if exists nglstudio;
+create database nglstudio ;
 
-DROP TABLE IF EXISTS PROJET ; 
-CREATE TABLE PROJET (
-	id_projet BIGINT  AUTO_INCREMENT NOT NULL, 
-	nom_projet VARCHAR(64), 
-	categorie_projet VARCHAR(16), 
-	description_projet TEXT, 
-	date_debut DATE, 
-	date_fin DATE, 
-	budget FLOAT, 
-	PRIMARY KEY (id_projet) ) ENGINE=InnoDB; 
+use nglstudio;
 
-DROP TABLE IF EXISTS TACHE ; 
-CREATE TABLE TACHE (
-	id_tache BIGINT  AUTO_INCREMENT NOT NULL, 
-	libelle_tache VARCHAR(64), 
-	deadline DATE, 
-	statut_tache VARCHAR(16), 
-	id_projet BIGINT NOT NULL, 
-	PRIMARY KEY (id_tache) ) ENGINE=InnoDB;  
+drop table if exists client;
+create table client (
+	id_client bigint auto_increment not null primary key,
+	prenom varchar(16), 
+	nom varchar(16), 
+	code_postal varchar(5), 
+	ville varchar(16), 
+	rue varchar(64)) ENGINE=InnoDB;  
 
-DROP TABLE IF EXISTS nglstudio;
-CREATE DATABASE nglstudio ;
+drop table if exists projet ; 
+create table projet (
+	id_projet bigint  auto_increment not null primary key, 
+	nom_projet varchar(64), 
+	categorie_projet varchar(16), 
+	description_projet text, 
+	date_debut date, 
+	date_fin date, 
+	budget float) ENGINE=InnoDB; 
 
-USE nglstudio;
+drop table if exists tache ; 
+create table tache (
+	id_tache bigint  auto_increment not null primary key, 
+	libelle_tache varchar(64), 
+	deadline date, 
+	statut_tache varchar(16), 
+	id_projet bigint not null,
 
-DROP TABLE IF EXISTS RESSOURCE ; 
-CREATE TABLE RESSOURCE (
-	id_ressource BIGINT  AUTO_INCREMENT NOT NULL, 
-	type_ressource VARCHAR(16), 
-	PRIMARY KEY (id_ressource) ) ENGINE=InnoDB;  
+	foreign key (id_projet) references projet (id_projet)) ENGINE=InnoDB;  
 
-DROP TABLE IF EXISTS NOTE ; 
-CREATE TABLE NOTE (id_note BIGINT  AUTO_INCREMENT NOT NULL, 
-	libelle_note VARCHAR(16), 
-	contenu_note TEXT, 
-	id_projet BIGINT NOT NULL, 
-	id_utilisateur BIGINT NOT NULL, 
-	PRIMARY KEY (id_note) ) ENGINE=InnoDB;  
+drop table if exists ressource ; 
+create table ressource (
+	id_ressource bigint  auto_increment not null primary key, 
+	type_ressource varchar(16)) ENGINE=InnoDB;  
 
-DROP TABLE IF EXISTS UTILISATEUR ; 
-CREATE TABLE UTILISATEUR (id_utilisateur BIGINT  AUTO_INCREMENT NOT NULL, 
-	nom_utilisateur VARCHAR(16), 
-	prenom_utilisateur BIGINT(16), 
-	mail_utilisateur BIGINT(512), 
-	passe_utilisateur VARCHAR(40), 
-	PRIMARY KEY (id_utilisateur) ) ENGINE=InnoDB;  
+drop table if exists utilisateur ; 
+create table utilisateur (
+	id_utilisateur bigint  auto_increment not null primary key, 
+	nom_utilisateur varchar(16), 
+	prenom_utilisateur varchar(16), 
+	mail_utilisateur varchar(512), 
+	passe_utilisateur varchar(40)) ENGINE=InnoDB;  
 
-DROP TABLE IF EXISTS CLIENT_PROJET ; 
-CREATE TABLE CLIENT_PROJET (
-	id_client BIGINT  AUTO_INCREMENT NOT NULL, 
-	id_projet BIGINT NOT NULL, 
-	PRIMARY KEY (id_client,  id_projet) ) ENGINE=InnoDB;  
+drop table if exists note ; 
+create table note (
+	id_note bigint  auto_increment not null primary key, 
+	libelle_note varchar(16), 
+	contenu_note text, 
+	id_projet bigint not null, 
+	id_utilisateur bigint not null,
 
-DROP TABLE IF EXISTS RESSOURCE_PROJET ; 
-CREATE TABLE RESSOURCE_PROJET (id_ressource BIGINT  AUTO_INCREMENT NOT NULL, 
-	id_projet BIGINT NOT NULL, 
-	montant_ressource INT, 
-	PRIMARY KEY (id_ressource,  id_projet) ) ENGINE=InnoDB;  
+	foreign key (id_projet) references projet (id_projet),
+	foreign key (id_utilisateur) references utilisateur (id_utilisateur)) ENGINE=InnoDB;  
 
-DROP TABLE IF EXISTS UTILISATEUR_TACHE ; 
-CREATE TABLE UTILISATEUR_TACHE (
-	id_utilisateur BIGINT  AUTO_INCREMENT NOT NULL, 
-	id_tache BIGINT NOT NULL, 
-	PRIMARY KEY (id_utilisateur,  id_tache) ) ENGINE=InnoDB;  
+drop table if exists client_projet ; 
+create table client_projet (
+	id_client bigint not null, 
+	id_projet bigint not null,
 
-DROP TABLE IF EXISTS UTILISATEUR_PROJET ; 
-CREATE TABLE UTILISATEUR_PROJET (
-	id_projet BIGINT  AUTO_INCREMENT NOT NULL, 
-	id_utilisateur BIGINT NOT NULL, 
-	PRIMARY KEY (id_projet,  id_utilisateur) ) ENGINE=InnoDB;
+	primary key (id_client, id_projet),
 
-ALTER TABLE TACHE ADD CONSTRAINT FK_TACHE_id_projet FOREIGN KEY (id_projet) REFERENCES PROJET (id_projet);
-ALTER TABLE NOTE ADD CONSTRAINT FK_NOTE_id_projet FOREIGN KEY (id_projet) REFERENCES PROJET (id_projet); 
-ALTER TABLE NOTE ADD CONSTRAINT FK_NOTE_id_utilisateur FOREIGN KEY (id_utilisateur) REFERENCES UTILISATEUR (id_utilisateur); 
-ALTER TABLE CLIENT_PROJET ADD CONSTRAINT FK_CLIENT_PROJET_id_client FOREIGN KEY (id_client) REFERENCES CLIENT (id_client); 
-ALTER TABLE CLIENT_PROJET ADD CONSTRAINT FK_CLIENT_PROJET_id_projet FOREIGN KEY (id_projet) REFERENCES PROJET (id_projet); 
-ALTER TABLE RESSOURCE_PROJET ADD CONSTRAINT FK_RESSOURCE_PROJET_id_ressource FOREIGN KEY (id_ressource) REFERENCES RESSOURCE (id_ressource); 
-ALTER TABLE RESSOURCE_PROJET ADD CONSTRAINT FK_RESSOURCE_PROJET_id_projet FOREIGN KEY (id_projet) REFERENCES PROJET (id_projet);
-ALTER TABLE UTILISATEUR_TACHE ADD CONSTRAINT FK_UTILISATEUR_TACHE_id_utilisateur FOREIGN KEY (id_utilisateur) REFERENCES UTILISATEUR (id_utilisateur); 
-ALTER TABLE UTILISATEUR_TACHE ADD CONSTRAINT FK_UTILISATEUR_TACHE_id_tache FOREIGN KEY (id_tache) REFERENCES TACHE (id_tache); 
-ALTER TABLE UTILISATEUR_PROJET ADD CONSTRAINT FK_UTILISATEUR_PROJET_id_projet FOREIGN KEY (id_projet) REFERENCES PROJET (id_projet); 
-ALTER TABLE UTILISATEUR_PROJET ADD CONSTRAINT FK_UTILISATEUR_PROJET_id_utilisateur FOREIGN KEY (id_utilisateur) REFERENCES UTILISATEUR (id_utilisateur);
+	foreign key (id_client) references client (id_client),
+	foreign key (id_projet) references projet (id_projet)) ENGINE=InnoDB;  
+
+drop table if exists ressource_projet ; 
+create table ressource_projet (
+	id_ressource bigint not null, 
+	id_projet bigint not null, 
+	montant_ressource int,
+
+	primary key (id_ressource, id_projet),
+
+	foreign key (id_ressource) references ressource (id_ressource),
+	foreign key (id_projet) references projet (id_projet)) ENGINE=InnoDB;  
+
+drop table if exists utilisateur_tache ; 
+create table utilisateur_tache (
+	id_utilisateur bigint not null, 
+	id_tache bigint not null,
+
+	primary key (id_utilisateur, id_tache),
+
+	foreign key (id_utilisateur) references utilisateur (id_utilisateur),
+	foreign key (id_tache) references tache (id_tache)) ENGINE=InnoDB;  
+
+drop table if exists utilisateur_projet ; 
+create table utilisateur_projet (
+	id_projet bigint not null, 
+	id_utilisateur bigint not null,
+
+	primary key (id_projet, id_utilisateur),
+
+	foreign key (id_projet) references projet (id_projet),
+	foreign key (id_utilisateur) references utilisateur (id_utilisateur)) ENGINE=InnoDB;    
